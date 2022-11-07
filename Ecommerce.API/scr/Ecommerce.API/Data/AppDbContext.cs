441afac1e8b10b19f9c9dd42105759de31535ae4
Matheus <matheuspedracone@gmail.com>
2022-11-07 19:31:04 -0300

    Feat: Mapeando entidades no AppDbContext

--- Ecommerce.API/scr/Ecommerce.API/Data/AppDbContext.cs
+++ Ecommerce.API/scr/Ecommerce.API/Data/AppDbContext.cs
@@ -6,9 +6,31 @@ namespace Ecommerce.API.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Establishment> Establishments { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(x => x.ProductCategory)
+                .WithMany(p => p.Products)
+                .HasForeignKey(pc => pc.ProductCategoryId);
+
+            modelBuilder.Entity<Establishment>()
+                .HasMany(p => p.Products);
+
+
+            modelBuilder.Entity<Establishment>()
+                .HasOne(p => p.EstablishmentCategory)
+                .WithMany(e => e.Market)
+                .HasForeignKey(x => x.EstablishmentCategoryId);
+
+            modelBuilder.Entity<Rating>().HasOne(e => e.Establishment).WithMany(r => r.Ratings);
+        }
     }
 }
