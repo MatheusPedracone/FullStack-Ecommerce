1998a70647e2c083517641d765e134046493b1ed
Matheus <matheuspedracone@gmail.com>
2022-11-07 19:31:43 -0300

    Feat: Adicionando banco de dados

--- Ecommerce.API/scr/Ecommerce.API/Program.cs
+++ Ecommerce.API/scr/Ecommerce.API/Program.cs
@@ -1,11 +1,28 @@
+using Ecommerce.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddControllers().AddNewtonsoftJson(x =>
+                {
+                    x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    x.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MarketPlace.Api", Version = "v1" });
+            });
+builder.Services.AddDbContext<AppDbContext>((x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}));
 
 var app = builder.Build();
 
@@ -13,9 +30,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ecommerce.Api v1"));
 }
 
+app.UseCors(policy => policy
+                             .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .SetIsOriginAllowed((host) => true)
+                                .AllowCredentials());
+ 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
